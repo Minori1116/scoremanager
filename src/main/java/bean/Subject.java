4d4b1621a67a97a6d537534c8e64e1fd16f00f5b
aaa <kmm2559383@stu.o-hara.ac.jp>
2026-04-24 10:53:36 +0900

    Merge branch 'master' of https://github.com/Minori1116/scoremanager.git

--- src/main/java/bean/Subject.java
+++ src/main/java/bean/Subject.java
@@ -1,16 +1,16 @@
 package bean;
 
 public class Subject {
-    private String cd; // 科目コード
-    private String name; // 科目名
-    private School School; // 学校名
+    private School school; // 学校コード
+    private String cd;       // 科目コード
+    private String name;    // 科目名
+    
+    public School getSchool() { return school; }
+    public void setSchool(School school) { this.school = school; }
 
     public String getCd() { return cd; }
     public void setCd(String cd) { this.cd = cd; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-
-    public School getSchool() { return School; }
-    public void setSchool(School school) { School = school; }
 }
